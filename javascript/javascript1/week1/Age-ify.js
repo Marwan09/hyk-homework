7f9113d750526526e5da5a4691c2eb34e55f1555
Marwan09 <marwan.shubat@email.com>
2022-11-26 13:09:04 +0100

    add html file and age ify exresis

---
+++ javascript/javascript1/week1/Age-ify.js
@@ -0,0 +1,13 @@
+ //Age-ify (A future age calculator)
+        let yearOfBirth = 1990;
+        let yearFuture = 2045
+        let futureyear = 2045 - yearOfBirth;
+   
+        //  let nowDate = new Date()
+        //  let age = nowDate.getFullYear() - yearOfBirth;
+        // console.log(` Your future age in 1990 is ${yearFuture}`);
+        // console.log(`Your age now is ${age}`);
+        console.log(`The type of first variable is ${typeof(yearOfBirth)}`);
+        console.log(`The type of seconed variable is ${typeof(yearFuture)}`);
+        let age = yearFuture - 1990;
+        console.log(`You will be "${age}" years old in " ${yearFuture}" `);
